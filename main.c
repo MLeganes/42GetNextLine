d888f28be4f52cb629fbced467b97b78f5ee5e7b
Angel Morcillo leganes <amorcill@1-B-8.42heilbronn.de>
2021-07-30 14:29:42 +0200

    main for testing

--- main.c
+++ main.c
@@ -5,25 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/28 18:20:51 by amorcill          #+#    #+#             */
-/*   Updated: 2021/07/28 19:13:31 by amorcill         ###   ########.fr       */
+/*   Created: 2021/07/30 11:24:19 by amorcill          #+#    #+#             */
+/*   Updated: 2021/07/30 12:01:05 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+* Lib fcntl.h (sys/fcntl.h) is for open(2) files with file status flag
+* O_RDONLY	0x0000	open-for-reading-only
+*/
+#include <fcntl.h> 
+
 #include "get_next_line.h"
+// needed?
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(void)
+/*
+* The main function is for testing porpouse. It is not a part of the proyect.
+* 
+*/
+int	main(void)
 {
-	size_t bytes;
-	// file
-	FILE *fp;
+	int		fd;
+	int		len;
+	char	*line;
 
-	fp = fopen("gnl_text.txt", "r");
-
-	bytes = fread()
-
-
-
-	fp = fclose(fp);
+	line = NULL;
+	fd = open("texts/gnl_text1.txt", O_RDONLY, 0);
+	if (fd < 0)
+	{
+		write(1, "Error occurred when try to open fd", 33);
+		return (-1);
+	}	
+	len = 1;
+	line = get_next_line(fd);
+	while (!line)
+	{
+		printf("%s", line);
+		line = get_next_line(fd);
+	}
+	close(fd);
 	return (0);
 }
